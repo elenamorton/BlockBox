d54b7144a69154451515355c463d264ba5bf6caa
elenamorton <elena.uleia@gmail.com>
2017-07-20 14:12:47 +0100

    added root route for server and feature test it.

---
+++ app/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let httpPort = process.env.HTTP_PORT || 3001
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello BlockBox!!')
+})
+
+app.listen(httpPort, () => console.log(`Listening http on port: ${httpPort}`))
---
+++ test/server.js
@@ -0,0 +1,16 @@
+const expect  = require('chai').expect
+const chaiHttp = require('chai-http')
+const http = require('http')
+
+describe('BlockBox Http server', () => {
+    describe('/', () => {
+    
+      it('should return status 200', (done) => {
+        http.get('http://localhost/3001', (response) => {
+          expect(response.statusCode).to.equal(200)
+          done()
+        })
+      })
+   })
+  
+})
