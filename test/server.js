3c59c96aa49ed0e1b27a780a98e28544c758768c
elenamorton <elena.uleia@gmail.com>
2017-07-20 14:22:43 +0100

    correction in the test http path. Test passing

--- test/server.js
+++ test/server.js
@@ -4,13 +4,11 @@ const http = require('http')
 
 describe('BlockBox Http server', () => {
     describe('/', () => {
-    
       it('should return status 200', (done) => {
-        http.get('http://localhost/3001', (response) => {
+        http.get('http://localhost:3001/', (response) => {
           expect(response.statusCode).to.equal(200)
           done()
         })
       })
    })
-  
 })
