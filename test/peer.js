51359cc312e25b3497c1969aef0aa23559447c78
elenamorton <elena.uleia@gmail.com>
2017-07-21 16:09:07 +0100

    added peer2 for server broadcast test

--- test/peer.js
+++ test/peer.js
@@ -6,14 +6,13 @@ const webSocket = require('ws')
 const Server = require('mock-socket').Server
 
 describe('BlockBox Web Socket Server', () => {
+  const mockServer = new Server('ws://localhost:6001')
+  const peer1 = new webSocket('ws://localhost:6002/')
+  const message = 'test message'
+    
   afterEach( (done) => {
     mockServer.stop(done)
   })
-  
-  const mockServer = new Server('ws://localhost:6001')
-  const peer1 = new webSocket('ws://localhost:6002/')
-
-  const message = 'test message'
 
   it('wc server can send data to peer 1', () => {
     mockServer.on('connection', server => {
@@ -21,13 +20,27 @@ describe('BlockBox Web Socket Server', () => {
       mockServer.send(message)
     })
     
-    peer1.once('message', (message) => {
-      expect(message).equal(JSON.stringify(message))
+    peer1.once('message', (peerMessage) => {
+      expect(peerMessage).equal(JSON.stringify(message))
     })
     
   })
   
-  
+  it('wc server can send data to peer 1 and peer 2', () => {
+    const peer2 = new webSocket('ws://localhost:6003/')
+    mockServer.on('connection', server => {
+      mockServer.send(message)
+      mockServer.send(message)
+    })
+    
+    peer1.once('message', (peerMessage) => {
+      expect(peerMessage).equal(JSON.stringify(message))
+    })
+    peer2.once('message', (peerMessage) => {
+      expect(peerMessage).equal(JSON.stringify(message))
+    })
+    
+  })
   
 })
   
